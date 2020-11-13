703fee7fb74202eebcb737290628e0561c1bbe03
jhumjhum12 <43745169+jhumjhum12@users.noreply.github.com>
2020-11-13 09:36:21 +0530

    Update db.php

--- www/db.php
+++ www/db.php
@@ -1,4 +1,4 @@
 <?php
     header("Access-Control-Allow-Origin: *");
-    $con = mysqli_connect("use1id.com.mysql:3306","use1id_comuse1idapp","app@use1id@","use1id_comuse1idapp") or die ("could not connect database");
+    $con = mysqli_connect("localhost","use1id_com","LVIS2019","use1id_com") or die ("could not connect database");
 ?>
