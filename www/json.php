957b3a4e14d56b3db259ab6fe309a74aa61da89c
jhumjhum12 <43745169+jhumjhum12@users.noreply.github.com>
2020-11-13 09:37:44 +0530

    Update json.php

--- www/json.php
+++ www/json.php
@@ -2,7 +2,7 @@
 echo 'hello';//exit;
 include "db.php";
 $data=array();
-$q=mysqli_query($con,"select * from `customers`");
+$q=mysqli_query($con,"select * from `ud_gen_pd_users`");
 while ($row=mysqli_fetch_object($q)){
  $data[]=$row;
 }
